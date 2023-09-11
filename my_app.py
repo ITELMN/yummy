46d66e50e1c3839295bcb81f95d20e4fcbb37df9
artor <59762138+elham-code@users.noreply.github.com>
2023-09-11 19:16:33 +0800

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -28,7 +28,18 @@ if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
+
+
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+msg = response.choices[0].message
+
+# 修改生成的回复内容，使其看起来像一个大学生的回应
+msg["content"] = "嘿，没问题！我认为你的问题是这样的：[用户问题]。在大学里，我们通常这样解决它：[建议或解决方法]。还有其他什么我可以帮助你的吗？"
+
+# 添加修改后的回复到对话历史
+st.session_state.messages.append({"role": "assistant", "content": msg["content"]})
+st.chat_message("assistant").write(msg["content"])
+    # response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    # msg = response.choices[0].message
+    # st.session_state.messages.append(msg)
+    # st.chat_message("assistant").write(msg.content)
