82d9c4c73cd8cddc4dd77c7c70103737431cad08
artor <59762138+elham-code@users.noreply.github.com>
2023-09-11 19:19:29 +0800

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -34,7 +34,7 @@ if prompt := st.chat_input():
     msg = response.choices[0].message
 
 # 修改生成的回复内容，使其看起来像一个大学生的回应
-    msg["content"] = "嘿，没问题！我认为你的问题是这样的：[用户问题]。在大学里，我们通常这样解决它：[建议或解决方法]。还有其他什么我可以帮助你的吗？"
+    msg["content"] = "嘿，我是你的大学生活一起度过的朋友，遇到什么问题了嘛？"
 
 # 添加修改后的回复到对话历史
     st.session_state.messages.append({"role": "assistant", "content": msg["content"]})
